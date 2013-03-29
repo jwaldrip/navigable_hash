6538ed45e1bc12dbc419a3ffa408772b2d067949
Jason Waldrip <jason@waldrip.net>
2013-03-28 22:53:35 -0600

    Update spec to support setters

--- spec/lib/navigable_hash_spec.rb
+++ spec/lib/navigable_hash_spec.rb
@@ -13,6 +13,64 @@ describe NavigableHash do
   let(:hash){ TEST_HASH }
   let(:navigable){ NavigableHash.new(hash) }
 
+  context "with a deep setter" do
+    let(:hash){ {} }
+    it "should be able to set and get a deep value" do
+      value = "world"
+      navigable.foo = {}
+      navigable.foo.bar = {}
+      navigable.foo.bar.baz = value
+      navigable.foo.bar.baz.should == value
+    end
+  end
+
+  describe "#[]=" do
+    it "should set a value" do
+      expect { navigable[:new_key] = 'value' }.to change { navigable[:new_key] }
+    end
+
+    context "given a hash" do
+      it "should return a new instance of navigable hash" do
+        hash = {a: 1, b: 2, c: 3}
+        navigable.should_receive(:navigate_hash).with(hash)
+        navigable[:hash] = hash
+      end
+    end
+
+    context "given an array" do
+      it "should call #navigate with each value" do
+        array = [1, 2, 3]
+        navigable.should_receive(:navigate_array).with(array)
+        navigable[:array] = array
+      end
+    end
+
+    context "given any object" do
+      it "should return a value" do
+        object = Object.new
+        navigable.should_receive(:navigate_value).with(object)
+        navigable[:object] = object
+      end
+    end
+
+    context "given nil" do
+      it "should return a value" do
+        navigable.should_receive(:navigate_value).with(nil)
+        navigable[:nil_value] = nil
+      end
+    end
+  end
+
+  describe "#any_method=" do
+    it "should not raise an error" do
+      expect { navigable.any_method = 'value' }.to_not raise_error
+    end
+
+    it "should set a value" do
+      expect { navigable.any_method = 'value' }.to change { navigable.any_method }
+    end
+  end
+
   describe "#[]" do
 
     context "given a hash" do
@@ -23,22 +81,18 @@ describe NavigableHash do
 
     context "given an array" do
       it "should call #navigate with each value" do
-        navigable.should_receive(:navigate_array).with(hash[:array])
-        navigable[:array]
+        navigable[:array].should be_a Array
       end
     end
 
     context "given any object" do
       it "should return a value" do
-        navigable.should_receive(:navigate_value).with(hash[:object]).any_number_of_times.and_call_original
-        navigable[:object].should == hash[:object]
+        navigable[:object].should be_an_instance_of Object
       end
     end
 
     context "given nil" do
       it "should return a value" do
-        navigable.should_receive(:navigate_value).with(hash[:nil_value]).any_number_of_times.and_call_original
-        navigable[:nil_value].should == hash[:nil_value]
         navigable[:nil_value].should be_nil
       end
     end
